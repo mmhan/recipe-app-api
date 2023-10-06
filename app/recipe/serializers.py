7f3873e4a167ebf0db87f242424662b45f035d61
Mike Myat Min Han <mmhan@indeed.com>
2023-10-06 11:53:08 +0100

    Added serializers and views for list and detail recipe API

---
+++ app/recipe/serializers.py
@@ -0,0 +1,23 @@
+"""
+Serializers for recipe APIs
+"""
+
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'link']
+        read_only_field = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
